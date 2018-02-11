3b34ddd0f68a2905481a1c517e5770c0db5b2910
Heather Park <hthr.prk@gmail.com>
2018-02-10 19:07:30 -0800

    Add Button component

--- src/components/container/CartlyProductOverview/CartlyProductOverview.js
+++ src/components/container/CartlyProductOverview/CartlyProductOverview.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ProductOverview } from '../../presentational';
+import { Button } from '../../presentational';
 
 export default function CartlyProductOverview({ product }) {
   return(
@@ -8,6 +9,7 @@ export default function CartlyProductOverview({ product }) {
         <img src={`images/${product.filename}`} alt={product.name}/>
         <p>{product.name}</p>
         <p>{product.price}</p>
+        <Button>Add to cart</Button>
       </ProductOverview>
     </div>
   )
--- src/components/presentational/index.js
+++ src/components/presentational/index.js
@@ -1,1 +1,2 @@
 export { default as ProductOverview } from './ProductOverview/ProductOverview';
+export { default as Button } from './Button/Button';
