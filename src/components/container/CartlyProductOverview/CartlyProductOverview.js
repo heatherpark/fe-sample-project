41b67d26de7e42a80afbd4389d7dab04e1660f9a
Heather Park <hthr.prk@gmail.com>
2018-02-10 18:25:03 -0800

    Create components for product overview UI

---
+++ src/components/container/CartlyProductOverview/CartlyProductOverview.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ProductOverview } from '../../presentational';
+
+export default function CartlyProductOverview() {
+  return(
+    <div>
+      <ProductOverview />
+    </div>
+  )
+}
