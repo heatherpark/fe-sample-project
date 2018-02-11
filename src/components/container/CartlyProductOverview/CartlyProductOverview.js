edb9b4ddf34bdb29808386f134dc44195720b2b4
Heather Park <hthr.prk@gmail.com>
2018-02-10 18:36:52 -0800

    Set up container component for product overview

--- src/components/container/CartlyProductOverview/CartlyProductOverview.js
+++ src/components/container/CartlyProductOverview/CartlyProductOverview.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { ProductOverview } from '../../presentational';
 
-export default function CartlyProductOverview(props) {
+export default function CartlyProductOverview({ product }) {
   return(
     <div>
-      {props.product.name}
-      <ProductOverview />
+      <ProductOverview>
+        <img src={`images/${product.filename}`} alt={product.name}/>
+        <p>{product.name}</p>
+        <p>{product.price}</p>
+      </ProductOverview>
     </div>
   )
 }
