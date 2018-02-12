3e23cf3bcdfa62e774b0266c47b466f7a52d75c2
Heather Park <hthr.prk@gmail.com>
2018-02-11 18:40:28 -0800

    Add total price to cart

--- src/components/container/Cart/Cart.js
+++ src/components/container/Cart/Cart.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { CartItem } from '../index';
 
 export default function Cart(props) {
+  let total = props.cart.reduce((total, item)=> {
+    return total + item.price;
+  }, 0);
+
   return (
     <div>
+      Total: ${total}
       {props.cart.map(item => {
         return (
           <CartItem item={item}
